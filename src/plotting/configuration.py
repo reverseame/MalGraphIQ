dd3ae7f6a3db6033af1d7ea54bcc78e5107e7aad
RazviOverflow <razvioverflow@gmail.com>
2024-12-30 17:04:17 +0100

    Added auxiliary files

---
+++ src/plotting/configuration.py
@@ -0,0 +1,34 @@
+radarchart_padding = {
+    "Filesystem":2,
+    "Memory":0,
+    "Operating\nSystem":15,
+    "Cryptography":25,
+    "Process":12,
+    "Communication":30
+}
+
+abbreviation_map = {
+    "Encryption Key":"EC",
+    "Cryptographic Hash":"CH",
+    "Encrypt Data":"ED",
+    "Decrypt Data":"DD",
+    "Get File Attributes":"GFA",
+    "Read File":"RF",
+    "Create or Open file":"COF",
+    "Alter Filename Extension":"AFE",
+    "Create Directory":"CD",
+    "Copy File":"CF",
+    "Delete File":"DF",
+    "Move File":"MF",
+    "Write File":"WF",
+    "Create Thread":"CT",
+    "Open Thread":"OT",
+    "Enumerate Threads":"ET",
+    "Process Enumeration":"PE",
+    "Resume Thread":"RT",
+    "Create Mutex":"CM",
+    "Suspend Thread":"ST",
+    "Create Process":"CP",
+    "Open Process":"OP",
+    "Check Mutex":"ChM",
+}
