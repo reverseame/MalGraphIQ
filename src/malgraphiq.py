46608dddde5712e930baa7dd66f2a27b2aaa17cd
RazviOverflow <razvioverflow@gmail.com>
2025-01-05 10:39:15 +0100

    Changing phases' names

--- src/malgraphiq.py
+++ src/malgraphiq.py
@@ -4,9 +4,9 @@ import sys
 from pathlib import Path
 
 # Import each phase
-from transition_matrix_and_graphs import main as transition_matrix_main
-from behavioral_pattern_occurrences import main as behavior_pattern_main
-from plot_catalog_matches import main as plot_matches_main
+import graphs
+import occurrences
+import plotting
 
 
 def parse_arguments():
@@ -21,7 +21,7 @@ def parse_arguments():
     subparsers = parser.add_subparsers(dest='phase', required=True, help="Specify the phase to run.")
 
     # Arguments for transition_matrix_and_graphs
-    parser_transition = subparsers.add_parser("transition_matrix", help="Transition Matrix and Graphs phase.")
+    parser_transition = subparsers.add_parser("graphs", help="Transition Matrix and Graphs phase.")
     parser_transition.add_argument("json_dir", help="Directory containing JSON reports.")
     parser_transition.add_argument("-o", "--output", default="REPORTS", help="Output folder.")
     parser_transition.add_argument("-w", "--winapi_categories", default="./winapi_categories.json", help="Path to winapi_categories.json.")
@@ -29,7 +29,7 @@ def parse_arguments():
     parser_transition.add_argument("-pp", "--print_transition_probabilities", action="store_true", help="Print transition probabilities on graphs.")
 
     # Arguments for behavioral_pattern_occurrences
-    parser_behavior = subparsers.add_parser("behavior_patterns", help="Behavior Pattern Occurrences phase.")
+    parser_behavior = subparsers.add_parser("occurrences", help="Behavior Pattern Occurrences phase.")
     parser_behavior.add_argument("behavior_graph", help="Behavior graph file or directory.")
     parser_behavior.add_argument("-c", "--catalog", required=True, help="Path to the behavior catalog JSON.")
     parser_behavior.add_argument("-m", "--max_inter_nodes", type=int, default=0, help="Max intermediate nodes allowed.")
@@ -38,7 +38,7 @@ def parse_arguments():
     parser_behavior.add_argument("-jf", "--json_output_file", help="Custom output JSON file for results.")
 
     # Arguments for plot_catalog_matches
-    parser_plot = subparsers.add_parser("plot_matches", help="Plot Catalog Matches phase.")
+    parser_plot = subparsers.add_parser("plots", help="Plot Catalog Matches phase.")
     parser_plot.add_argument("json", help="JSON file or directory of matches.")
     parser_plot.add_argument("--fig_title", help="Title of the generated figure.")
     parser_plot.add_argument("-rc_max", "--radarchart_max_scale", type=int, default=100, choices=range(0, 101), metavar="[0-100]", help="Max scale for radarcharts.")
